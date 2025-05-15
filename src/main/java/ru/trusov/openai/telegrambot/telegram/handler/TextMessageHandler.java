a0c71236abf795bef6f3b6fc9d8944c1821b886d
Maxim <123011969+TrusovMaxim@users.noreply.github.com>
2025-05-15 21:25:13 +0300

    refactor: extract premium active message to constants

--- src/main/java/ru/trusov/openai/telegrambot/telegram/handler/TextMessageHandler.java
+++ src/main/java/ru/trusov/openai/telegrambot/telegram/handler/TextMessageHandler.java
@@ -4,10 +4,16 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.trusov.openai.telegrambot.constant.BotErrors;
+import ru.trusov.openai.telegrambot.constant.BotMessages;
 import ru.trusov.openai.telegrambot.model.entity.User;
 import ru.trusov.openai.telegrambot.model.enums.UserActionPathEnum;
 import ru.trusov.openai.telegrambot.service.bot.MessageSenderService;
 import ru.trusov.openai.telegrambot.service.user.UserService;
+
+import java.text.MessageFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 @Component
 @AllArgsConstructor
@@ -20,7 +26,14 @@ public class TextMessageHandler {
         var chatId = update.getMessage().getChatId();
         var text = update.getMessage().getText();
         if ("/buy_premium".equalsIgnoreCase(text)) {
-            messageSenderService.sendPremiumInvoice(chatId);
+            if (user != null && Boolean.TRUE.equals(user.getIsPremium())
+                    && user.getPremiumEnd() != null
+                    && LocalDateTime.now().isBefore(user.getPremiumEnd())) {
+                var until = user.getPremiumEnd().format(DateTimeFormatter.ofPattern("dd MMMM yyyy", new Locale("ru")));
+                messageSenderService.send(MessageFormat.format(BotMessages.MESSAGE_PREMIUM_ALREADY_ACTIVE, until), chatId);
+            } else {
+                messageSenderService.sendPremiumInvoice(chatId);
+            }
             return;
         }
         var action = UserActionPathEnum.parse(text);
